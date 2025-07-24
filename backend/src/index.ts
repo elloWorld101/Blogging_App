e9fd6e507ba1a2fe7b5c05fb2e537e9fb275ce72
unknown <rithvikbansal0501@gmail.com>
2025-07-24 12:45:49 +0530

    Routes Added and Log error solved

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,34 @@
 import { Hono } from 'hono'
 
+
 const app = new Hono()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+app.post('/api/v1/signup', (c) => {
+  return c.text('Signup Route');
 })
+
+app.post("/api/v1/signin", (c)=>{
+  return c.text("Signin Route");
+})
+
+app.post("/api/v1/blog", (c)=>{
+  return c.text("Blog Post")
+})
+
+app.put("/api/v1/blog", (c)=>{
+  return c.text("Blog Put");
+})
+
+app.get("/api/v1/blog/:id", (c)=>{
+  const id = c.req.param("id");
+  console.log(id);
+  return c.text("Blog with Route param");
+})
+
+app.get("/api/get/blog/bulk", (c)=>{
+  return c.text("Blog Bulk");
+})
+
+
 
 export default app
