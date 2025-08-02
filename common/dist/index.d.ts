a451d4a17af1b1acaf1c0f7ae2323facb1525916
unknown <rithvikbansal0501@gmail.com>
2025-08-02 21:07:28 +0530

    Added gitignore and compiled the typescript file

---
+++ common/dist/index.d.ts
@@ -0,0 +1,22 @@
+import zod from "zod";
+export declare const signupInput: zod.ZodObject<{
+    email: zod.ZodEmail;
+    password: zod.ZodString;
+    name: zod.ZodOptional<zod.ZodString>;
+}, zod.core.$strip>;
+export type SignupInput = zod.infer<typeof signupInput>;
+export declare const signinInput: zod.ZodObject<{
+    email: zod.ZodEmail;
+    password: zod.ZodString;
+}, zod.core.$strip>;
+export type SigninInput = zod.infer<typeof signinInput>;
+export declare const createPostInput: zod.ZodObject<{
+    title: zod.ZodString;
+    content: zod.ZodString;
+}, zod.core.$strip>;
+export type CreatePostInput = zod.infer<typeof createPostInput>;
+export declare const updatePostInput: zod.ZodObject<{
+    title: zod.ZodString;
+    content: zod.ZodString;
+}, zod.core.$strip>;
+export type updatePostInput = zod.infer<typeof updatePostInput>;
