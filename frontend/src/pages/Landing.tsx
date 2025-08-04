220f002fa9a01843876eedf7db3ef6c093fe4b9d
unknown <rithvikbansal0501@gmail.com>
2025-08-04 18:36:58 +0530

    Added html to Landing page and also added the image

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,7 +1,20 @@
 export function Landing(){
     return(
-        <div>
-            Landing page
+        <div className="h-screen bg-[#F7F4ED]">
+            <div className="flex justify-around py-5 border-1">
+                <h2 className=" text-md md:text-3xl font-semibold">ManseLikh</h2>
+                <div className="text-sm  md:text-lg">
+                    <a href="/signup" className="">Signup</a>
+                    <a href="/signin">Signin</a>
+                </div>
+            </div>
+
+            <div>
+                <h2>Human stories & ideas</h2>
+                <p>A place to read, write, and deepen your understanding</p>
+                <a href="/signup">Start Reading</a>
+                <img src="" alt="" />
+            </div>
         </div>
     )
 }
