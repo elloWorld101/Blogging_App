addd4040af74440d9c7294ecdf6af578f725720c
unknown <rithvikbansal0501@gmail.com>
2025-08-05 08:40:34 +0530

    Mobile version of landing page ready

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -5,22 +5,22 @@ export function Landing(){
         <div className="h-screen bg-[#F7F4ED]">
             
             <div className="flex flex-row justify-between py-5">
-                <div className="basis-2/3 mx-[5%]">
-                    <h2 className="text-3xl font-semibold px-">ManseLikh</h2>
+                <div className="md:basis-2/3 mx-[5%]">
+                    <h2 className="text-2xl  md:text-3xl font-bold md:font-semibold">ManseLikh</h2>
                 </div>
                 
                 
-                <div className="text-sm md:text-lg flex flex-row gap-5 basis-1/3 ">
-                    <a href="/signin" className="py-2 text-sm font-semibold">Sign in</a>
-                    <button className="bg-neutral-900 rounded-full text-white px-5 text-sm font-semibold">
+                <div className="text-sm md:text-lg flex flex-row gap-5 md:basis-1/3 ">
+                    <a href="/signin" className="hidden md:block py-2 text-sm font-semibold">Sign in</a>
+                    <button className="bg-neutral-900 rounded-full text-white px-5 text-sm font-semibold mr-3 md:mx-none">
                         <a href="/signup">Get Started</a>
                     </button>
                 </div>
             </div>
 
-            <div className="flex justify-between border-1 items-center flex-row "> 
+            <div className="flex justify-between border-1 items-center flex-row h-screen md:h-auto"> 
                 <div className="mt-5 font-domine mx-[5%] basis-2/3">
-                    <h2 className="text-9xl" >Human <br /> stories & ideas</h2>
+                    <h2 className= "text-7xl md:text-9xl" >Human <br /> stories & ideas</h2>
                     <p className="text-xl mt-7 font-roboto">A place to read, write, and deepen your understanding</p>
                     <button className="bg-neutral-900 rounded-full px-10 py-2 mt-10 text-white">
                         <a href="/signup" className="text-xl">Start Reading</a>
@@ -28,9 +28,13 @@ export function Landing(){
                 </div>
                 <div className="pt-5">
                     <img src={image} alt="" 
-                    className="h-150 w-110"/>
+                    className="hidden h-150 w-110 md:block"/>
                 </div>
                 
+            </div>
+
+            <div>
+
             </div>
         
         </div>
