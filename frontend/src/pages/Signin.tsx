805cc32b7b1fa71904de7f64595b504967d02e28
unknown <rithvikbansal0501@gmail.com>
2025-08-05 11:26:26 +0530

    Signin page design for web ready

--- frontend/src/pages/Signin.tsx
+++ frontend/src/pages/Signin.tsx
@@ -1,7 +1,26 @@
+import { Heading } from "../components/Heading"
+import { InputBox } from "../components/InputBox"
+import { SideBar } from "../components/SideBar"
+import { Button } from "../components/Button"
+
 export function Signin(){
     return(
-        <div>
-            Signin Page
+        <div className="flex h-screen">
+            <div className="basis-1/2 flex flex-col">
+                <div className="w-[50%] m-auto">
+                    <Heading heading="Sign in to your account" subheading="Don't have an account? " page="/signup" link="Sign up"/>
+                    <InputBox title="Username" placeholder="m@example.com" />
+                    <InputBox title="Password" placeholder="" />   
+                    <Button content="Sign In" />
+                </div>
+            </div>
+
+            <div className="bg-gray-200 basis-1/2 flex flex-col justify-center">
+                <div className=" px-[15%]">
+                    <SideBar heading='"The customer service I recieved was exceptional. The support team went above and beyond to address my concerns."'  subheading="Jules Winnfield" footer = "CEO, Acme Inc"/>
+                </div>
+            </div>
+
         </div>
     )
 }
