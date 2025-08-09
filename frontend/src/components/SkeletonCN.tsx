6b3290c429dd29880d0111023858a871e2814ae9
unknown <rithvikbansal0501@gmail.com>
2025-08-09 11:18:45 +0530

    Skeleton Ready

--- frontend/src/components/SkeletonCN.tsx
+++ frontend/src/components/SkeletonCN.tsx
@@ -4,11 +4,14 @@ export function SkeletonCN() {
   return (
     <div className="space-y-4 mt-20">
       <div className="flex gap-10 justify-center">
-        <div>
+        <div className="space-y-4">
           <div className="flex items-center space-x-4 space-y-4">
-            <Skeleton className="h-12 w-12 rounded-full bg-gray-200 " />
+            <div className="my-auto"> 
+              <Skeleton className="h-8 w-8 rounded-full bg-gray-200 " />
+            </div>
+            
             <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px] bg-gray-200" />
+              <Skeleton className="h-3 w-[125px] bg-gray-200 rounded-none" />
             </div>
           </div>
           
@@ -19,7 +22,7 @@ export function SkeletonCN() {
         </div>
 
         <div>
-          <Skeleton className="h-[150px] w-[150px] rounded-none bg-gray-200" />
+          <Skeleton className="h-[140px] w-[150px] rounded-none bg-gray-200" />
         </div>
       </div>
       
@@ -27,11 +30,14 @@ export function SkeletonCN() {
 
 
       <div className="flex gap-10 justify-center">
-        <div>
+        <div className="space-y-4">
           <div className="flex items-center space-x-4 space-y-4">
-            <Skeleton className="h-12 w-12 rounded-full bg-gray-200 " />
+            <div className="my-auto"> 
+              <Skeleton className="h-8 w-8 rounded-full bg-gray-200 " />
+            </div>
+            
             <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px] bg-gray-200" />
+              <Skeleton className="h-3 w-[125px] bg-gray-200 rounded-none" />
             </div>
           </div>
           
@@ -42,18 +48,21 @@ export function SkeletonCN() {
         </div>
 
         <div>
-          <Skeleton className="h-[150px] w-[150px] rounded-none bg-gray-200" />
+          <Skeleton className="h-[140px] w-[150px] rounded-none bg-gray-200" />
         </div>
       </div>
 
 
 
       <div className="flex gap-10 justify-center">
-        <div>
+        <div className="space-y-4">
           <div className="flex items-center space-x-4 space-y-4">
-            <Skeleton className="h-12 w-12 rounded-full bg-gray-200 " />
+            <div className="my-auto"> 
+              <Skeleton className="h-8 w-8 rounded-full bg-gray-200 " />
+            </div>
+            
             <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px] bg-gray-200" />
+              <Skeleton className="h-3 w-[125px] bg-gray-200 rounded-none" />
             </div>
           </div>
           
@@ -64,19 +73,21 @@ export function SkeletonCN() {
         </div>
 
         <div>
-          <Skeleton className="h-[150px] w-[150px] rounded-none bg-gray-200" />
+          <Skeleton className="h-[140px] w-[150px] rounded-none bg-gray-200" />
         </div>
       </div>
 
 
 
-
-      <div className="flex gap-10 justify-center ">
-        <div>
+      <div className="flex gap-10 justify-center">
+        <div className="space-y-4">
           <div className="flex items-center space-x-4 space-y-4">
-            <Skeleton className="h-12 w-12 rounded-full bg-gray-200 " />
+            <div className="my-auto"> 
+              <Skeleton className="h-8 w-8 rounded-full bg-gray-200 " />
+            </div>
+            
             <div className="space-y-2">
-              <Skeleton className="h-4 w-[250px] bg-gray-200" />
+              <Skeleton className="h-3 w-[125px] bg-gray-200 rounded-none" />
             </div>
           </div>
           
@@ -87,7 +98,7 @@ export function SkeletonCN() {
         </div>
 
         <div>
-          <Skeleton className="h-[150px] w-[150px] rounded-none bg-gray-200" />
+          <Skeleton className="h-[140px] w-[150px] rounded-none bg-gray-200" />
         </div>
       </div>
     </div>
